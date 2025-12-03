3f5ba6ad7679e1fb3656bc2a6c67bb061e38f56b
Kagimon <karlosralbuquerque@gmail.com>
2025-12-03 07:43:17 -0300

    Adicionando projeto Maven

---
+++ src/main/java/com/example/spotifytunado1/SpotifyTunado1Application.java
@@ -0,0 +1,13 @@
+package com.example.spotifytunado1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyTunado1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyTunado1Application.class, args);
+    }
+
+}
